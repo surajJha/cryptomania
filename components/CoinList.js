3604d8e3b7487892c198c8994869ffc7804ef4c9
surajjha <suraj.jha@bookmyshow.com>
2017-12-20 09:14:41 +0530

    added icon images to all coins

--- components/CoinList.js
+++ components/CoinList.js
@@ -3,43 +3,43 @@
  */
 import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
-import { Container, Text, StyleProvider, Header, Spinner, Content, List, ListItem } from 'native-base';
-import TextTheme from '../native-base-theme/components/Text';
-import _ from 'lodash';
+import { Container, Text, StyleProvider, Header, Spinner, Content, List, ListItem, Thumbnail, Left, Body, Right, } from 'native-base';
+
 export default class CoinList extends Component {
-	constructor() {
-		super();
-		this.state = {
-			prices: [],
-			loading: true
-		}
-	}
+	state = {
+		'prices': [],
+		'loading': true
+	};
+
+	mapping = {
+		'BTC': {'name':'Bitcoin', 'imgPath': require('../assets/images/Bitcoin.png')},
+		'ETH': {'name':'Ethereum', 'imgPath': require('../assets/images/Ethereum.png')},
+		'BCH': {'name':'Bitcoin Cash', 'imgPath': require('../assets/images/bitcoinCash.png')},
+		'XRP': {'name':'Ripple', 'imgPath': require('../assets/images/Ripple.png')},
+		'LTC': {'name':'Litecoin', 'imgPath': require('../assets/images/Litecoin.png')},
+		'MIOTA': {'name':'Iota', 'imgPath': require('../assets/images/Iota.png')},
+		'OMG': {'name':'OmiseGo', 'imgPath': require('../assets/images/OmiseGo.png')},
+		'GNT': {'name':'Golem', 'imgPath': require('../assets/images/Golem.png')}
+	};
 
 	async componentDidMount() {
 		let tickerData = await fetch('https://koinex.in/api/ticker');
-		this.setState({loading: false});
-		//console.log(tickerData._bodyText);
-		tickerData = JSON.parse(tickerData._bodyText);
-		let prices = tickerData.prices;
-		let data = [];
-		for(key in prices) {
-			if(tickerData.hasOwnProperty(key)) {
-				let currency = {
-					currency: key,
-					price: prices[key]
-				}
-				console.log(key, prices[key]);
-				data.push(currency);
-			}
+		tickerData = await tickerData.json();
+
+		const tickerPrices = tickerData.prices;
+		const prices = [];
+		for(const key in tickerPrices) {
+			prices.push({
+				'currency': this.mapping[key].name,
+				'price': tickerPrices[key],
+				'imgPath': this.mapping[key].imgPath
+			});
 		}
-		//console.log(this.state.prices);
-		console.log(data);
-		//console.log(_.values(prices));
-		this.setState({prices: data});
 
-		console.log(this.state.prices);
-
-
+		this.setState({
+			prices,
+			'loading': false
+		});
 	}
 
 	render() {
@@ -49,36 +49,38 @@ export default class CoinList extends Component {
 			)
 		}
 		return (
-		<Container>
-			<Header centerComponent={{ text: 'Currencies', style: { color: '#fff' } }}/>
-			<Content>
-				<List dataArray={this.state.prices} renderRow={(item) =>
-					<ListItem>
-						<Text>{item.currency} - {item.price}</Text>
-					</ListItem>
-				}>
-
-				</List>
-			</Content>
-		</Container>
-
-
-
-	);
+			<Container>
+				<Header centerComponent={{ 'text': 'Currencies', 'style': { 'color': '#fff' } }}/>
+				<Content>
+					<List dataArray={this.state.prices} renderRow={(item) =>
+						<ListItem style={{padding:5}} avatar>
+							<Left>
+								<Thumbnail size={90} source={item.imgPath} />
+							</Left>
+							<Body>
+							<Text>{item.currency} - inr {item.price}</Text>
+							</Body>
+							<Right></Right>
+						</ListItem>
+					}>
+					</List>
+				</Content>
+			</Container>
+		);
 	}
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
+	'container': {
+		'flex': 1,
 	},
-	header: {
-		height: 20,
-		backgroundColor: '#bb4d22'
+	'header': {
+		'height': 20,
+		'backgroundColor': '#bb4d22'
 	},
-	spinnerContainer: {
-		justifyContent: 'center',
-		alignItems: 'center',
-		flex: 1
+	'spinnerContainer': {
+		'justifyContent': 'center',
+		'alignItems': 'center',
+		'flex': 1
 	}
 });
